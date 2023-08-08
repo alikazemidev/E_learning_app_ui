d7fe9f639de8038113be06d6cc1c28c83f1e9201
alikz <ali6316k@gmail.com>
2023-08-08 11:20:36 +0330

    add folder and constants colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xfffcfcfc);
+const Color kPrimaryLightColor = Color(0xffedf6f3);
+const Color kPrimaryColor = Color(0xffdaefe8);
+const Color kPrimaryDarkColor = Color(0xff88a1ae);
+const Color kAccentColor = Color(0xfffdca73);
+const Color kFontColor = Color(0xff252435);
+const Color kFontLightColor = Color(0xffb4b7bf);
